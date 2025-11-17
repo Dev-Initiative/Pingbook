e8fcbfe08079b10619fd2f274c1c65ff90bf8daa
Dude-dev1 <stephenyiadomosei@gmail.com>
2025-11-17 22:20:53 +0000

    Pushing minor changes

--- backend/server.ts
+++ backend/server.ts
@@ -1,12 +1,12 @@
 import dotenv from "dotenv";
-import express from "express";
+import express, { Request, Response } from "express";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Contact management system backend says hello.");
 });
 
